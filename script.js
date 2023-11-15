570f07f73da9a292b902463db56c3bb9d6e3f839
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-11-15 22:24:44 +0530

    add displayBoard function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Testing...');
+const TicTacToe = (function () {
+    const BOARD = [
+        ['X', 'O', 'X'],
+        ['X', 'X', 'O'],
+        ['O', 'O', 'X'],
+    ];
+    const displayBoard = function () {
+        let displayStr = '';
+        for (let i = 0; i < BOARD.length; i++) {
+            row = BOARD[i];
+            displayStr += row[0] + ' | ' + row[1] + ' | ' + row[2];
+            if (i == 2) return displayStr;
+            displayStr += '\n' + '-'.repeat(10) + '\n';
+        }
+    };
+    return {displayBoard};
+})();
+
