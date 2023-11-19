f8ff0b25b56a823133bef995e64b62e93eb46bc1
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-11-19 11:03:28 +0530

    add gameStatus logic for rowStatus & tie

--- script.js
+++ script.js
@@ -21,13 +21,32 @@ const TicTacToe = (function () {
         if (BOARD[pos[0]][pos[1]] == ' ') BOARD[pos[0]][pos[1]] = Player.marker;
         else console.log(`Already occupied by ${BOARD[pos[0]][pos[1]]}`);
     };
-    return { displayBoard, makeMove };
+    const gameStatus = function (Player) {
+        const rowStatus = BOARD.some((i) => i.every((j) => j == Player.marker));
+        // const colStatus = BOARD;
+        const tie = BOARD.every((i) => i.every((j) => j != ' '));
+        if (rowStatus) return 1;
+        // else if (colStatus) return 1;
+        else if (tie) return 0;
+        else return -1;
+    };
+    return { displayBoard, makeMove, gameStatus };
 })();
 
 const P1 = Player('Durgesh', 'X');
 const P2 = Player('Babu', 'O');
-console.log(TicTacToe.displayBoard());
+
+TicTacToe.makeMove(P2, [0, 0]);
+TicTacToe.makeMove(P1, [0, 1]);
+TicTacToe.makeMove(P1, [0, 2]);
+
+TicTacToe.makeMove(P1, [1, 0]);
+TicTacToe.makeMove(P2, [1, 1]);
 TicTacToe.makeMove(P1, [1, 2]);
+
+TicTacToe.makeMove(P1, [2, 0]);
+TicTacToe.makeMove(P1, [2, 1]);
+TicTacToe.makeMove(P2, [2, 2]);
+
 console.log(TicTacToe.displayBoard());
-TicTacToe.makeMove(P2, [1, 2]);
-console.log(TicTacToe.displayBoard());
+console.log(TicTacToe.gameStatus(P1));
