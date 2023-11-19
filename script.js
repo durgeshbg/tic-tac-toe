dab9381330925fd614692cf7efed730665a509cd
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-11-19 12:35:10 +0530

    add comments and playerCreate logic

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const Player = function (name, marker) {
-    return { name, marker };
-};
-
 const TicTacToe = (function () {
     const BOARD = [
         [' ', ' ', ' '],
@@ -9,6 +5,22 @@ const TicTacToe = (function () {
         [' ', ' ', ' '],
     ];
 
+    const players = [];
+
+    // Player object
+    const Player = function (name, marker) {
+        return { name, marker };
+    };
+    // function: add players in game
+    const playerCreate = function (name, marker) {
+        if (players.length >= 2) {
+            console.log('Cannot add for than two players!');
+            return;
+        }
+        players.push(Player(name, marker));
+    };
+
+    // function: display board state
     const displayBoard = function () {
         let displayStr = '';
         for (let i = 0; i < BOARD.length; i++) {
@@ -19,22 +31,26 @@ const TicTacToe = (function () {
         }
     };
 
+    // function: make move by a specified Player in position vector
     const makeMove = function (Player, pos) {
         if (BOARD[pos[0]][pos[1]] == ' ') BOARD[pos[0]][pos[1]] = Player.marker;
         else console.log(`Already occupied by ${BOARD[pos[0]][pos[1]]}`);
     };
 
+    // function: checks if rows match in board for Player
     const checkRows = function (board, Player) {
         return board.some((i) => i.every((j) => j == Player.marker));
     };
 
+    // function: transpose rows and cols of copied BOARD
     const transposeBoard = function () {
         duplicate = JSON.parse(JSON.stringify(BOARD));
         for (let i = 0; i < 3; i++)
             for (let j = 0; j < 3; j++) duplicate[i][j] = BOARD[j][i];
         return duplicate;
     };
-    
+
+    // function: checks both diagonals match
     const checkDiagonals = function (Player) {
         let d1 = 0,
             d2 = 0;
@@ -42,10 +58,10 @@ const TicTacToe = (function () {
             d1 = BOARD[i][i] == Player.marker ? d1 + 1 : d1;
             d2 = BOARD[i][2 - i] == Player.marker ? d2 + 1 : d2;
         }
-
         return d1 == 3 ? true : d2 == 3 ? true : false;
     };
 
+    // function: game state of Player (0: tie, 1: win, -1:continue)
     const gameStatus = function (Player) {
         const rowStatus = checkRows(BOARD, Player);
         const colStatus = checkRows(transposeBoard(), Player);
@@ -57,23 +73,10 @@ const TicTacToe = (function () {
         else return -1;
     };
 
-    return { displayBoard, makeMove, gameStatus };
+    return { displayBoard, makeMove, gameStatus, players, playerCreate };
 })();
 
-const P1 = Player('Durgesh', 'X');
-const P2 = Player('Babu', 'O');
-
-TicTacToe.makeMove(P1, [0, 0]);
-TicTacToe.makeMove(P2, [0, 1]);
-TicTacToe.makeMove(P2, [0, 2]);
-
-TicTacToe.makeMove(P1, [1, 0]);
-TicTacToe.makeMove(P2, [1, 1]);
-TicTacToe.makeMove(P2, [1, 2]);
-
-TicTacToe.makeMove(P2, [2, 0]);
-TicTacToe.makeMove(P1, [2, 1]);
-TicTacToe.makeMove(P1, [2, 2]);
-
-console.log(TicTacToe.displayBoard());
-console.log(TicTacToe.gameStatus(P2));
+TicTacToe.playerCreate('Durgesh', 'X');
+TicTacToe.playerCreate('Babu', 'O');
+TicTacToe.playerCreate('Babu', 'O');
+console.log(TicTacToe.players);
